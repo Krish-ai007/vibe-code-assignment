484d0520688547f5b169faf04b384d969493c1e8
Krish-ai007 <krishpachchigar9@gmail.com>
2026-06-30 15:59:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { SearchPage } from "@/pages/SearchPage";
 import { ProfileDetailPage } from "@/pages/ProfileDetailPage";
 import { ListPage } from "@/pages/ListPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SearchPage />} />
         <Route path="/profile/:username" element={<ProfileDetailPage />} />
         <Route path="/list" element={<ListPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
